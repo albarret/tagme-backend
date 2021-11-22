942264c1ba9a79c5b7d390d346b6cfd600b813ce
Aleson Barreto <alesonbg@gmail.com>
2021-11-21 23:39:37 -0300

    construção API

--- server/controller/asset.controller.js
+++ server/controller/asset.controller.js
@@ -1,44 +1,82 @@
-var Asset = require('../model/asset.model');
+var Asset = require("../model/asset.model");
 
 exports.create = (req, res) => {
-    if(!req.body) {
-        res.status(400).send({ message: "Content cannot be empty!"});
-        return;
-    }
+  if (!req.body) {
+    res.status(400).send({ message: "Content cannot be empty!" });
+    return;
+  }
 
-    const body = req.body;
+  const body = req.body;
 
-    const asset = new Asset({
-        name: body.name,
-        description: body.description,
-        health: body.health,
-        status: body.status,
-        img: body.img,
-        model: body.model,
-        owner: body.owner,
-        unit: body.unit
+  const asset = new Asset({
+    name: body.name,
+    description: body.description,
+    health: body.health,
+    status: body.status,
+    img: body.img,
+    model: body.model,
+    owner: body.owner,
+    unit: body.unit,
+  });
+
+  asset
+    .save(asset)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error ocurred while crating new Asset",
+      });
     });
-
-    asset
-        .save(asset)
-        .then(data => {
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error ocurrde while crating new Asset"
-            });
-        });
-}
+};
 
 exports.find = (req, res) => {
-    
-}
+  Asset.find()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error ocurred while finding Asset",
+      });
+    });
+};
 
 exports.update = (req, res) => {
-    
-}
+  if (!req.body) {
+    return res.status(400).send({ message: "Data to update cannot be empty" });
+  }
+
+  const id = req.params.id;
+  Asset.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update asset with id ${id}. Maybe asset cannot be found!`,
+        });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error updating asset information." });
+    });
+};
 
 exports.delete = (req, res) => {
-    
-}
+  const id = req.params.id;
+  Asset.findByIdAndDelete(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete asset with id ${id}. Maybe asset cannot be found!`,
+        });
+      } else {
+        res.send({ message: "Asset deleted successfully!" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error deleting asset with id" + id });
+    });
+};
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const services = require('../services/render');
+const services = require("../services/render");
+const assetControler = require("../controller/asset.controller");
 
+route.get("/", services.homeRoutes);
 
-route.get('/', services.homeRoutes); 
+// API
+route.post("/api/assets", assetControler.create);
+route.get("/api/assets", assetControler.find);
+route.put("/api/assets/:id", assetControler.update);
+route.delete("/api/assets/:id", assetControler.delete);
 
 module.exports = route;
